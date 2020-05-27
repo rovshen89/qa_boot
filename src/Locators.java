fdb590b42521b63add48a585ea45b73c08f639d7
rovshen89 <rovshen89@gmail.com>
2020-05-27 06:23:23 -0500

    Locate elements

--- src/Locators.java
+++ src/Locators.java
@@ -9,12 +9,19 @@ public class Locators {
         driver.manage().window().maximize();
         driver.get("https://www.bankofamerica.com");
         Thread.sleep(2000);
-        driver.findElement(By.id("engagementBannerCloseBtn")).click();
+        driver.findElement(By.id("engagementBannerCloseBtn")).click(); //find element by id
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//a[@aria-controls='navCheckingContent']")).click();
-        Thread.sleep(3000);
-        driver.findElement(By.name("query")).sendKeys("test message");
+        driver.findElement(By.xpath("//a[@aria-controls='navCheckingContent']")).click(); // find element by xpath
         Thread.sleep(2000);
+        driver.findElement(By.name("query")).sendKeys("test message"); //find element by name attribute
+        Thread.sleep(2000);
+        driver.findElement(By.className("search-query")).clear();
+        Thread.sleep(2000);
+        driver.findElement(By.className("search-query")).sendKeys("a new test message");
+        Thread.sleep(2000);
+        driver.findElement(By.linkText("Wealth Management")).click();
+        Thread.sleep(2000);
+        //driver.findElement(By.partialLinkText("Management")).click(); //we can use partial link text also
         driver.quit();
 
     }
