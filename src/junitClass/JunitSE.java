2251af11dc0ed0dc8421e990b57559d7a6fcae95
rovshen89 <rovshen89@gmail.com>
2020-05-31 14:11:15 -0500

    JSExecutor

--- src/junitClass/JunitSE.java
+++ src/junitClass/JunitSE.java
@@ -1,21 +1,26 @@
 package junitClass;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
 
 import static junitClass.JunitSelenium.baseUrl;
 
+
+
+@TestMethodOrder(MethodOrderer.Alphanumeric.class)
 public class JunitSE {
     static WebDriver driver;
     static String baseURL;
     static JavascriptExecutor jse;
+    private Object WebElement;
 
     @BeforeAll
     public static void setup(){
@@ -40,13 +45,48 @@ public class JunitSE {
         System.out.println("Quit all active windows");
     }
 
-    @Test
-    public void test1() throws InterruptedException {
+    @Test//replacement for driver click
+    public void click() throws InterruptedException {
         Thread.sleep(3000);
         WebElement checking = driver.findElement(By.xpath("//a[@aria-controls = 'navCheckingContent']"));
         jse.executeScript("arguments[0].click();", checking);
         System.out.println("Test1 execution");
     }
 
+    @Test//replacement for driver sendKeys
+    public void sendKeys() throws InterruptedException {
+        Thread.sleep(3000);
+        WebElement onlineID = driver.findElement(By.xpath("//input[@aria-labelledby='labelForonlineId1']"));
+//        Actions act = driver.findElement(By.xpath("//input[@aria-labelledby='labelForonlineId1']"))
+        jse.executeScript("arguments[0].value='tester1';", onlineID);
+        Thread.sleep(5000);
+        jse.executeScript("arguments[0].value=arguments[1]", onlineID, " tester2");
+        System.out.println("Test1 execution");
+    }
+
+    @Test
+    public void scroll() throws InterruptedException {
+        Thread.sleep(3000);
+        jse.executeScript("window.scrollBy(0,1000)");
+        Thread.sleep(3000);
+        jse.executeScript("window.scrollBy(0,-1000)");
+        Thread.sleep(3000);
+        System.out.println("Test scroll executed");
+    }
+
+    @Test
+    public void scrollView() throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement agree =  (WebElement) jse.executeScript("return document.getElementById(arguments[0])", "labelForonlineId1");
+        jse.executeScript("arguments[0].scrollIntoView(true)", agree);
+    }
+
+    @Test
+    public void windowSize() throws InterruptedException {
+        Thread.sleep(2000);
+        long height = (Long) jse.executeScript("return window.innerHeight");
+        long width = (Long) jse.executeScript("return window.innerWidth");
+        System.out.println("Window dimensions are: " + height + "x" + width);
+    }
 
 }
