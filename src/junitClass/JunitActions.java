3c1a4ebe5ce47371a9f654c47d53671b8e334605
rovshen89 <rovshen89@gmail.com>
2020-06-03 17:10:05 -0500

    Actions Class

--- src/junitClass/JunitActions.java
+++ src/junitClass/JunitActions.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
 /*
 * Create Actions action object. Then perform actions defined by webelements. */
 public class JunitActions {
@@ -34,7 +37,7 @@ public class JunitActions {
 
 
     @Test
-    public void test1() throws InterruptedException {
+    public void hoverOver_click() throws InterruptedException {
         WebElement hovSupport = driver.findElement(By.xpath("//section//a[text()='Support']"));
         WebElement forumClick = driver.findElement(By.xpath("//a[text()='Forums']"));
 
@@ -43,10 +46,28 @@ public class JunitActions {
         action.moveToElement(forumClick).click().perform(); //to click on a given Web element
         Thread.sleep(3000);
 
-        String currURL = driver.getCurrentUrl();
+        String currURL = driver.getCurrentUrl(); //check expected vs current URL
         Assertions.assertEquals("https://forum.jquery.com/", currURL, "URL mismatch. Current URL is: " + currURL);
         System.out.println("Current URL is: " + currURL);
         System.out.println("@Test1 - executed test");
+    }
+
+    @Test
+    public void doubleClick_contextClick() throws InterruptedException {
+        driver.navigate().to("https://jqueryui.com/button/");
+        Thread.sleep(3000);
+
+        WebElement frame = driver.findElement(By.cssSelector(".demo-frame"));
+        driver.switchTo().frame(frame);
+
+        WebElement anchor = driver.findElement(By.xpath("//div[@class = 'widget']//a[text() = 'An anchor']"));
+        action.doubleClick(anchor).perform();
+        Thread.sleep(3000);
+        action.contextClick(anchor).perform();
+
+        System.out.println("Double click and Right click successfull");
+
+
     }
 
 }
