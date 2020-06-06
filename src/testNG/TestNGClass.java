2b8ab90f415e3f422ebd4e67d9ef4cc53dd9d4a2
rovshen89 <rovshen89@gmail.com>
2020-06-06 11:58:25 -0500

    TestNG Class methods

---
+++ src/testNG/TestNGClass.java
@@ -0,0 +1,41 @@
+package testNG;
+
+import com.sun.security.jgss.GSSUtil;
+import org.testng.annotations.*;
+
+public class TestNGClass {
+    @BeforeClass
+    public void setUp(){
+        System.out.println("@BeforeClass method");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        System.out.println("@AfterClass method");
+    }
+
+    @BeforeMethod
+    public void init(){
+        System.out.println("@BeforeMethod method");
+    }
+
+    @AfterMethod
+    public void quit(){
+        System.out.println("@AfterMethod method");
+    }
+
+    @Test
+    public void baseNG(){
+        System.out.println("Base Test");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("Test1 method");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test2 method");
+    }
+}
