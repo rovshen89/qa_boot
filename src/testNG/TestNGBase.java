6a872f2e5a7fbb8533842fab18f7e9a29b1a0638
rovshen89 <rovshen89@gmail.com>
2020-06-06 09:04:33 -0500

    TestNG bases

---
+++ src/testNG/TestNGBase.java
@@ -0,0 +1,21 @@
+package testNG;
+
+import org.testng.annotations.Test;
+
+public class TestNGBase {
+
+    @Test
+    public void baseNG(){
+        System.out.println("Base Test");
+    }
+
+    @Test
+    public void test1(){
+        System.out.println("Test1 method");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test2 method");
+    }
+}
