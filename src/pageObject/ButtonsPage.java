b2451cd3a6ef45a73b9ec4be66e9a72d81d8d5fa
rovshen89 <rovshen89@gmail.com>
2020-06-14 13:40:40 -0500

    TestNG Page Object

--- src/pageObject/ButtonsPage.java
+++ src/pageObject/ButtonsPage.java
@@ -2,6 +2,7 @@ package pageObject;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
 public class ButtonsPage {
 
@@ -9,10 +10,23 @@ public class ButtonsPage {
     //WebElement submit = driver.findElement(By.xpath("//input[@value = 'A submit button'])position()=1]"));
     //WebElement anchor = driver.findElement(By.xpath("//a[text() = 'An anchor'])position()=1]"));
 
-    @FindBy
+    @FindBy(how = How.XPATH, using = "(//button[text() = 'A button element'])position()=1]")
     WebElement buttonElem;
+
+    @FindBy(how = How.XPATH, using = "(//input[@value = 'A submit button'])position()=1]")
     WebElement submit;
+
+    @FindBy(how = How.LINK_TEXT, using = "An anchor")
     WebElement anchor;
+
+    @FindBy(how = How.LINK_TEXT, using = " view source")
+    WebElement viewSource;
+
+    @FindBy(how = How.CLASS_NAME, using = "demo-frame")
+    WebElement frame;
+
+    @FindBy(how = How.ID, using = "content")
+    WebElement content;
 
 
 }
